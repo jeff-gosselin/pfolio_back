56ee2abb0ec625810c8b03d9a7df3db62b945257
Jeff <jgoz.email@gmail.com>
2019-06-10 21:17:59 -0400

    finishing up JWT Auth

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::ProjectsController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
+
+    def index
+        @projects = Project.all 
+        render json: @projects
+    end
+
+    def show
+        @project = Project.find(:params[:id])
+        render json: @project
+    end
+
+    def create
+        @project = Project.create(project_params)
+
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:title, :description, :url, :technologies, :date)
+    end
 end
