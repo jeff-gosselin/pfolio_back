68c5aedeb9c127ac6149b447e4936a6b48f65fe7
jeff-gosselin <jgoz.email@gmail.com>
2019-09-25 15:31:18 -0400

    fixed issue with bio pic not displaying when logged out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :projects, dependent: :destroy
     has_many :skills, dependent: :destroy
     has_one :summary, dependent: :destroy
-    has_one_attached :pic
+    has_one_attached :pic, dependent: :destroy
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
 end
