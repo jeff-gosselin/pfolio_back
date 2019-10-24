26dc62ac288626a3987092a023d2e7a2ade61227
jeff-gosselin <jgoz.email@gmail.com>
2019-10-24 12:23:58 -0400

    CORS

--- config/application.rb
+++ config/application.rb
@@ -21,12 +21,12 @@ module PfolioBack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
+      end
+    end
     config.api_only = true
   end
 end
